57f5dbfe1867c20b3b4f26d5ffd5880fbe57b586
deepalisrivastva12 <115392693+deepalisrivastva12@users.noreply.github.com>
2024-08-25 11:42:34 +0530

    Add files via upload

---
+++ variable.kt
@@ -0,0 +1,26 @@
+import com.sun.jdi.IntegerType
+
+fun main(){
+    //types of variable are var and val
+    //val is a constant variable we
+    val b=90
+    //b=80
+    println(b)
+
+    var a: Int
+    a=78
+    println(a)
+    var A=100
+    println(A)
+
+    var String=45
+    println(String)
+
+    var age: Int = 25 // another method of assigning values to variable
+    println(age)
+
+    var age2: Int //declaring variable
+    age2=18
+    println(age2)
+
+}
