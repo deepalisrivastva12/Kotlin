81a596a448c7ddf66f449dc15cc5bb6f5e5be56c
deepalisrivastva12 <115392693+deepalisrivastva12@users.noreply.github.com>
2024-08-25 11:41:59 +0530

    Add files via upload

---
+++ lec1.kt
@@ -0,0 +1,25 @@
+fun main(){
+
+    println("Hello, world!!!") // this will print the next statement in the new line
+    print("Hi friends")
+
+    // this is single line comment
+    /* this is
+      multiple line comment*/
+
+    var a =50
+
+   // a=40.5
+    //above line will give error because in the above line kotlin has already defined the data-type of "a" which is "int" so,
+    // we can not change the data type of variable in next-line by giving the direct value
+
+     println(a)
+    //we cannot give the keyword as a variable name and it is case-sensitive
+
+//    var try=40
+//    print(try)
+
+     var Try = 40 //this will not give error
+     print( Try)
+
+}
