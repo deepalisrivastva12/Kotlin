bbac47c5534a92fa4ba2fbf69ac6aae8568ddebe
deepalisrivastva12 <115392693+deepalisrivastva12@users.noreply.github.com>
2024-08-27 23:39:40 +0530

    Add files via upload

---
+++ Calculator.kt
@@ -0,0 +1,17 @@
+fun main(){
+
+    var choice=4
+
+    var a =4
+    var b =6
+
+    when(choice){
+        1 -> println("Addition is: "+ (a+b))
+        2 -> println("Substraction is: "+(a-b))
+        3-> println("Multiplication is: "+ (a*b))
+        4 -> println("Divivsion is: "+(a/b))
+        5 -> println("Modulus is : "+(a%b))
+        else -> println("Invalid coice")
+        
+    }
+}
